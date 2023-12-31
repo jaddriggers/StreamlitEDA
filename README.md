725f6b440eae9571cd076e0a49eca9d5b0114e98
Jad Driggers <jaddriggers@gmail.com>
2023-12-31 10:25:12 -0500

    Add docstrings and create README
    Added docstrings to methods in `StreamlitEDA.py` for better code readability and understanding. Created a `README.md` to provide instructions and details about the Streamlit Data Analyzer application.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Streamlit Data Analyzer
+
+This is a basic Streamlit application that provides an easy way to analyze data in your pandas dataframe.
+
+## Features:
+
+- Visualize the sum of null values in your dataframe across all columns.
+
+## Instructions:
+
+1. Ensure you have Streamlit installed. If not, you can install it with pip:
+
+   ```bash
+   pip install streamlit
+   ``` 
+2. Import your dataframe (named df in this example).
+3. Run this command in your terminal to start the application:
+
+   ```bash
+   streamlit run app.py
+   ```
+   
+4. Navigate to the localhost URL provided in the terminal output to interact with the application.
+
+## Modules Used:
+
+- Streamlit: For the web application interface.
+- Pandas: For handling and analyzing the dataframe.
+
+## Code:
+
+The primary logic for analyzing null values in the dataframe is implemented with this line of code:
--- StreamlitEDA.py
+++ StreamlitEDA.py
@@ -10,6 +10,12 @@ DATETIME_TYPE = 'datetime64[ns]'
 SUMMARY_MESSAGE = "Display summary statistics of the dataframe:"
 
 def read_file(uploaded_file):
+    """
+    Reads a file and returns its contents as a Pandas DataFrame.
+
+    :param uploaded_file: The file to be read. This can be either a CSV file or a ZIP file containing a single CSV file.
+    :return: A Pandas DataFrame containing the contents of the file.
+    """
     if zipfile.is_zipfile(uploaded_file):
         with zipfile.ZipFile(uploaded_file) as z:
             with z.open(z.namelist()[0]) as f:
@@ -19,6 +25,18 @@ def read_file(uploaded_file):
 
 
 def change_dtypes(df, col_to_change, new_type):
+    """
+    Change the data type of a specific column in a DataFrame.
+
+    :param df: The DataFrame in which the column's data type needs to be changed.
+    :type df: pandas.DataFrame
+    :param col_to_change: The name of the column to change its data type.
+    :type col_to_change: str
+    :param new_type: The new data type to be assigned to the column. Supported values: 'datetime', 'int', 'float', 'object', 'bool'.
+    :type new_type: str
+    :return: The DataFrame with the updated data type for the specified column.
+    :rtype: pandas.DataFrame
+    """
     if new_type == 'datetime':
         new_type = DATETIME_TYPE
     df[col_to_change] = df[col_to_change].astype(new_type)
@@ -26,6 +44,16 @@ def change_dtypes(df, col_to_change, new_type):
 
 # EXTRACT FUNCTION for plotting
 def plot_columns(df, columns_to_plot, column_type):
+    """
+    :param df: DataFrame containing the data to be plotted.
+    :param columns_to_plot: List of column names to be plotted.
+    :param column_type: Type of the columns to be plotted. Possible values are "Numerical" or "Categorical".
+    :return: None
+
+    This method plots the specified columns from the given DataFrame based on their type. If the column type is "Numerical", a pair plot is created using seaborn library and displayed using
+    * streamlit library. If the column type is "Categorical", individual count plots are created for each column using seaborn library and displayed using streamlit library. If no columns
+    * are available for plotting based on the given column type, a message is displayed using streamlit library.
+    """
     if columns_to_plot:
         if column_type == "Numerical":
             fig = sns.pairplot(data=df[columns_to_plot])
@@ -40,6 +68,12 @@ def plot_columns(df, columns_to_plot, column_type):
 
 
 def visualize_data(df):
+    """
+    Visualizes numerical or categorical columns in a given DataFrame.
+
+    :param df: Pandas DataFrame to visualize.
+    :return: None.
+    """
     numerical_columns = df.select_dtypes(include=['float64', 'int']).columns.tolist()
     categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
 
